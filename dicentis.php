5bfe4a309ced51d973f705559611ab3232617db3
Hans-Helge Buerger <mail@hig-podcast.de>
2013-11-22 18:17:29 +0100

    Add Dashboard and reorder submenu
    Add a new page to Podcast menu for a future Dashboard and use
    admin_init hook to run a method menu_order which reorders
    Podcast's submenu to put 'Dashboard' at top of the menu.

--- dicentis.php
+++ dicentis.php
@@ -20,7 +20,7 @@ if( !class_exists('Dicentis') ) {
 			// Load the plugin's translated strings
 			add_action( 'init' , array( $this, 'load_localisation' ) );
 
-			add_action( 'admin_init', array( $this, 'admin_init') );
+			add_filter( 'admin_init', array( $this, 'admin_init' ) );
 			add_action( 'admin_menu', array( $this, 'add_menu') );
 
 			// Initilize Settings
@@ -30,8 +30,72 @@ if( !class_exists('Dicentis') ) {
 			// Create CPT Podcast
 			require_once( sprintf("%s/classes/post-type-podcast.php", dirname(__FILE__)) );
 			$Dicentis_Podcast_CPT = new Dicentis_Podcast_CPT();
-		}
+		} // END public function __construct()
 
+		/**
+		 * Hook into WP's admin_init hook and do some admin stuff
+		 * 		1. reorder Podcast's submenu
+		 */
+		public function admin_init() {
+			$this->menu_order();
+		} // END public function admin_init()
+
+		/**
+		 * Add admin menu pages to podcast post type
+		 */
+		public function add_menu() {
+			add_submenu_page(
+				'edit.php?post_type=podcast', // add to podcast menu
+				__( 'Dashboard' ),
+				__( 'Dashboard' ),
+				'edit_posts',
+				'dicentis_dashboard',
+				array( $this, 'dicentis_dashboard_page' )
+			);
+		} // END public function add_menu()
+
+		public function dicentis_dashboard_page() {
+			if ( !current_user_can('edit_post') ) {
+				wp_die( __('You do not have sufficient permissions to access this page.') );
+			}
+
+			// Render the dashboard template
+			include( sprintf( "%s/templates/dashboard.php", dirname(__FILE__) ) );
+		} // END public function dicentis_dashboard_page()
+
+		/**
+		 * create a custom menu order to display the dashboard
+		 * menu always at the top of this post type
+		 */
+		public function menu_order() {
+			// $submenu contains the order of the complete menu
+			// i.e. top-level menus and submenus
+			global $submenu;
+
+			// Look for $find_page and the submenu $find_sub
+			$find_page = 'edit.php?post_type=podcast';
+			$find_sub  = 'Dashboard';
+
+			// Loop thru $submenu until $find_page is found
+			foreach ( $submenu as $page => $items ) :
+				if ( $page == $find_page ) :
+					// loop thru $find_page item and look for
+					// $find_sub b/c we want to reorder it
+					foreach ( $items as $id => $meta ) :
+						if ( $meta[0] == $find_sub ) :
+							// $find_sub is found so assing it to
+							// first place (0-based) in sub-array and unset
+							// its former entry.
+							// Last but not least sort it again.
+							// 'Dashboard' is now at the top of this submenu
+							$submenu[$find_page][0] = $meta;
+							unset( $submenu[$find_page][$id] );
+							ksort( $submenu[$find_page] );
+						endif;
+					endforeach;
+				endif;
+			endforeach;
+		} // END public function menu_order() 
 
 		public function load_localisation() {
 			load_plugin_textdomain( 'dicentis', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
---
+++ templates/dashboard.php
@@ -0,0 +1,4 @@
+<div class="wrap">
+	<div id="icon-themes" class="icon32"></div><h2>Dashboard</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ergo opifex plus sibi proponet ad formarum quam civis excellens ad factorum pulchritudinem? Qui enim existimabit posse se miserum esse beatus non erit. Color egregius, integra valitudo, summa gratia, vita denique conferta voluptatum omnium varietate. Callipho ad virtutem nihil adiunxit nisi voluptatem, Diodorus vacuitatem doloris. Hic nihil fuit, quod quaereremus. Frater et T. Et harum quidem rerum facilis est et expedita distinctio. Duo Reges: constructio interrete. Tria genera bonorum; Qui autem de summo bono dissentit de tota philosophiae ratione dissentit.</p>
+</div>
