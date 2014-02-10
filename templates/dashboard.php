11dcd022612d72f80bde7f72ecbef73be1b827dd
Hans-Helge Buerger <mail@hig-podcast.de>
2014-02-10 12:58:31 +0100

    Show all Feed links of all shows
    get taxonomy 'podcast_show' and loop thru it to get all slugs. Using a
    function it wrappes it into the predefined link

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,4 +1,44 @@
+<?php
+function dp_get_feed_link( $show_slug, $feed = 'rss' ) {
+	$show_feed_rss2 = trailingslashit( get_home_url() ) .
+		"?post_type=podcast&podcast_show=" . $show_slug . "&feed=rss2";
+	$show_feed_itunes = trailingslashit( get_home_url() ) .
+		"?post_type=podcast&podcast_show=" . $show_slug . "&feed=itunes";
+
+	switch ( $feed ) {
+		case 'itunes':
+			$link = $show_feed_itunes;
+			break;
+
+		case 'rss':
+		default:
+			$link = $show_feed_rss2;
+			break;
+	}
+
+	return $link;
+}
+?>
 <div class="wrap">
 	<div id="icon-themes" class="icon32"></div><h2>Dashboard</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ergo opifex plus sibi proponet ad formarum quam civis excellens ad factorum pulchritudinem? Qui enim existimabit posse se miserum esse beatus non erit. Color egregius, integra valitudo, summa gratia, vita denique conferta voluptatum omnium varietate. Callipho ad virtutem nihil adiunxit nisi voluptatem, Diodorus vacuitatem doloris. Hic nihil fuit, quod quaereremus. Frater et T. Et harum quidem rerum facilis est et expedita distinctio. Duo Reges: constructio interrete. Tria genera bonorum; Qui autem de summo bono dissentit de tota philosophiae ratione dissentit.</p>
+	<p><?php _e('This Dashboard provides you alle the needed information about your podcast shows.', 'dicentis'); ?></p>
+
+	<?php
+		if ( taxonomy_exists( 'podcast_show' ) ) {
+			$terms = get_terms('podcast_show');
+			foreach ( $terms as $show ) { ?>
+				<div class="dp_dashboard_feeds">
+				<?php foreach ( $show as $key => $value) {
+					if ( !strcmp( $key, 'name' ) ) { ?>
+							<h4><?php echo $value; ?></h4>
+					<?php } else if ( !strcmp( $key, 'slug' ) ) { ?>
+							<p class="dp_dashboard_feeds_rss"><?php echo dp_get_feed_link( $value, 'rss' ); ?></p>
+							<p class="dp_dashboard_feeds_itunes"><?php echo dp_get_feed_link( $value, 'itunes' ); ?></p>
+					<?php }
+				} ?>
+				</div>
+			<?php }
+		}
+	?>
+
 </div>
