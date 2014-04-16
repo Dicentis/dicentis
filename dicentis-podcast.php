dba5768765143780127c69f50ee19d996668fd8c
Hans-Helge Buerger <mail@hig-podcast.de>
2014-04-16 23:56:02 +0200

    add TGM plugin to recomend GitHub Updater Plugin

--- dicentis-podcast.php
+++ dicentis-podcast.php
@@ -14,6 +14,8 @@ Copyright 2013 Hans-Helge Buerger (http://hanshelgebuerger.de)
 License: GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
  */
 
+include_once 'lib/tgm-plugin.php';
+
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if( !class_exists('Dicentis') ) {
