174b11ea56cc5643822b30a6b8bc2c5e15ede532
Hans-Helge Buerger <mail@hig-podcast.de>
2014-05-23 09:31:57 +0200

    Fix: remove import limit of 5 episodes
    All Episodes of a feed should now be imported

--- classes/feed-import.php
+++ classes/feed-import.php
@@ -44,7 +44,7 @@ class FeedImport extends RSS {
 		if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
 			// Figure out how many total items there are, but limit it to 5. 
-			$maxitems = $rss->get_item_quantity( 5 ); 
+			$maxitems = $rss->get_item_quantity(); 
 
 			// Build an array of all the items, starting with element 0 (first element).
 			$rss_items = $rss->get_items( 0, $maxitems );
