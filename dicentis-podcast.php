c5c60ac42ba2755e33817b7186071bbdac0c60b2
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2014-06-28 21:43:55 +0200

    Refactore Dicentis_Podcast
    * remove unneeded variables
    * add PHPDocs
    * remove activation_hook
    * remove deactivation_hook

--- dicentis-podcast.php
+++ dicentis-podcast.php
@@ -1,21 +1,30 @@
 <?php
-/*
-Plugin Name: Dicentis Podcast
-Plugin URI: http://hanshelgebuerger.de
-Description: Manage multiple podcasts with ease in one plugin
-Version: 0.1.0
-Author: Hans-Helge Buerger
-Author URI: http://hanshelgebuerger.de
-Text Domain: dicentis
-Domain Path: /languages/
-GitHub Plugin URI: https://github.com/obstschale/dicentis-podcast
-GitHub Branch: master
-GitHub Access Token: 06f0508db3f02e704d0e1bbdce452be7d50aa308
-
-Copyright 2013 Hans-Helge Buerger (http://hanshelgebuerger.de)
-License: GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
+/**
+ * @package   Dicentis Podcast
+ * @author    Hans-Helge Buerger <mail@hanshelgebuerger.de>
+ * @license   GPL-3.0
+ * @link      http://dicentis.io
+ * @copyright 2014 Hans-Helge Buerger
+ *
+ * Plugin Name: Dicentis Podcast
+ * Plugin URI: http://hanshelgebuerger.de
+ * Description: Manage multiple podcasts with ease in one plugin
+ * Version: 0.1.0
+ * Author: Hans-Helge Buerger
+ * Author URI: http://hanshelgebuerger.de
+ * Text Domain: dicentis
+ * Domain Path: /languages/
+ * GitHub Plugin URI: https://github.com/obstschale/dicentis-podcast
+ * GitHub Branch: master
+ * GitHub Access Token: 06f0508db3f02e704d0e1bbdce452be7d50aa308
+ *
+ * Copyright 2014 Hans-Helge Buerger (http://hanshelgebuerger.de)
+ * License: GPL (http://www.gnu.org/licenses/old-licenses/gpl-3.0.txt)
  */
-namespace Dicentis;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 require_once __DIR__ . '/dicentis-define.php';
 
@@ -25,26 +34,20 @@ require_once DIPO_INC_DIR . '/taxonomies/show.php';
 require_once DIPO_LIB_DIR . '/tgm-plugin.php';
 require_once DIPO_LIB_DIR . '/dipo-version.php';
 
-if ( ! class_exists( 'Dicentis_Podcast' ) ) {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-dicentis-podcast.php';
-}
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-
 function dipo_load_plugin() {
 
 	$path = plugin_dir_path( __FILE__ );
 
 	if ( ! class_exists( 'Dipo_Load_Controller' ) ) {
 		require $path . 'includes/autoload/class-dipo-load-controller.php';
+		$loader = new \Dicentis\Autoload\Dipo_Load_Controller( $path . 'includes' );
 	}
 
-	$loader = new Autoload\Dipo_Load_Controller( $path . 'includes' );
-
-	$dipo = new Dicentis_Podcast();
-	$dipo->run();
+	if ( ! class_exists( 'Dicentis_Podcast' ) ) {
+		require_once plugin_dir_path( __FILE__ ) . 'includes/class-dicentis-podcast.php';
+		$dipo = new \Dicentis\Dicentis_Podcast();
+		$dipo->run();
+	}
 
 }
 dipo_load_plugin();
--- includes/class-dicentis-podcast.php
+++ includes/class-dicentis-podcast.php
@@ -7,17 +7,19 @@ use Dicentis\Podcast_Post_Type\Dipo_Podcast_Post_Type;
 use Dicentis\Feed\Dipo_RSS;
 use Dicentis\Core;
 
+/**
+ * The primary class for Dicentis Podcast.
+ * 
+ * This class is called by the plugin file dicentis-podcast.php and is the
+ * first class which is instantiated. It is responsible for loading the
+ * necessary hooks and create an object for the Podcast Custom Post Type.
+ *
+ * @author Hans-Helge Buerger <mail@hanshelgebuerger.de>
+ * @since 0.2.0
+ * @package Dicentis
+ * @subpackage Core
+ */
 class Dicentis_Podcast {
-
-	/**
-	 * Dicentis Settings Object which is responsible for all settings
-	 * regarding dicentis.
-	 * 
-	 * @since  0.2.0
-	 * @access private
-	 * @var Dipo_Settings $settings Dipo_Settings object
-	 */
-	private $settings;
 
 	/**
 	 * The core of this plugin is its Custom Post Type "Podcast"
@@ -31,16 +33,6 @@ class Dicentis_Podcast {
 	private $podcast_cpt;
 
 	/**
-	 * RSS Object for creating, instantiating a valid RSS Feed
-	 * 
-	 * @since  0.2.0
-	 * @access private
-	 * @var Dipo_RSS $feed adds new feeds to WordPress and is responsible
-	 *      for rendering the feeds
-	 */
-	private $feed;
-
-	/**
 	 * Loader class for holding all action and filter hooks in arrays
 	 * 
 	 * @since  0.2.0
@@ -51,111 +43,115 @@ class Dicentis_Podcast {
 	private $hook_loader;
 
 	/**
-	 * Admin object which is responsible for the backend pages
-	 * 
-	 * @since  0.2.0
-	 * @access private
-	 * @var [type]
+	 * Constructor loads dependencies and registers hooks.
+	 *
+	 * @see Dicentis_Podcast::load_dependencies()   instantiates necessary
+	 *                                              objects
+	 * @see Dicentis_Podcast::register_hooks()      registers important hooks
 	 */
-	private $admin;
-
 	public function __construct() {
 
-		$this->settings     = new Dipo_Settings();
-		$this->podcast_cpt  = new Dipo_Podcast_Post_Type();
-		$this->feed         = new Dipo_RSS();
-		$this->hook_loader  = new Core\Dipo_Hook_Loader();
-		$this->admin        = new Core\Dipo_Admin_Manager();
-
+		$this->load_dependencies();
 		$this->register_hooks();
+
+	}
+
+	/**
+	 * Load dependencies: Hook Loader and Podast CPT.
+	 *
+	 * @see Dipo_Hook_Loader        Loader object for registering filter
+	 *                              and action hooks
+	 * @see Dipo_Podcast_Post_Type  Custom post type object for podcasts
+	 */
+	public function load_dependencies() {
+
+		$this->hook_loader = new Core\Dipo_Hook_Loader();
+		$this->podcast_cpt  = new Dipo_Podcast_Post_Type();
+
+	}
+
+	/**
+	 * Method registers serveral hooks for this plugin.
+	 *
+	 * The Dipo_Hook_Loader object is used to register action and filter
+	 * hooks for Admin, Settings, Localization, RSS.
+	 * 
+	 * @see Dipo_Hook_Loader    Loader object for registering filter
+	 *                          and action hooks
+	 * @see Dipo_Admin_Manager  Admin object for pages in backend
+	 * @see Dipo_Settings       Settings object for saving and rendering
+	 * @see Dipo_Localization   L10n object loads textdomain
+	 * @see Dipo_RSS            Feed object for generating feeds
+	 */
+	public function register_hooks() {
 
 		// add_filter( 'single_template', array( $this, 'single_template' ) );
 		// add_filter( 'archive_template', array( $this, 'podcast_archive_template' ) );
 
-		add_filter( 'plugin_action_links_dicentis-podcast/dicentis-podcast.php', array( $this->settings, 'plugin_action_settings_link' ) );
-	} // END public function __construct()
-
-	public function register_hooks() {
 		/**
 		 * Admin Hooks
 		 */
+		$admin = new Core\Dipo_Admin_Manager();
 		$this->hook_loader->add_filter( 'admin_init',
-			$this->admin,
+			$admin,
 			'admin_init' );
 
 		$this->hook_loader->add_action( 'admin_menu',
-			$this->admin,
+			$admin,
+			'add_menu' );
+
+		/**
+		 * Settings Hooks
+		 */
+		$settings = new Dipo_Settings();
+		$this->hook_loader->add_filter(
+			'plugin_action_links_dicentis-podcast/dicentis-podcast.php',
+			$settings,
+			'plugin_action_settings_link' );
+
+		$this->hook_loader->add_action( 'admin_init',
+			$settings,
+			'admin_init' );
+
+		$this->hook_loader->add_action( 'admin_menu',
+			$settings,
 			'add_menu' );
 
 		/**
 		 * Localization Hooks
 		 */
+		$localization = new Core\Dipo_Localization();
 		$this->hook_loader->add_action( 'plugins_loaded',
-			new Core\Dipo_Localization(),
+			$localization,
 			'load_localisation' );
 
 		/**
 		 * RSS / Feed Hooks
 		 */
+		$feed = new Dipo_RSS();
 		$this->hook_loader->add_action( 'init',
-			$this->feed,
+			$feed,
 			'add_podcast_feeds');
 
 		$this->hook_loader->add_action( 'template_redirect',
-			$this->feed,
+			$feed,
 			'generate_podcast_feed' );
+
 	}
-
-
 
 	/**
-	 * Activate the plugin
-	 * this plugin needs at least WP v3.6
+	 * Getting Shit Real.
+	 *
+	 * After instantiating a Dicentis_Podcast object, loading all dependencies,
+	 * and regestering hooks in the loader this function sets all in motions
+	 * and finally registers all hooks for real. After this run the plugin
+	 * is fully functional.
+	 * 
+	 * @see Dipo_Hook_Loader  Loader object for registering filter
+	 *                        and action hooks
 	 */
-	public static function activate() {
-		// If WP version is not > 3.6 this plugin dies and cannot be used
-		if ( version_compare( get_bloginfo( 'version' ), '3.6', '<' ) ) {
-			// deactivate plugin
-			die( __( 'This Plugin requires WordPress version 3.6 or higher.', DIPO_TEXTDOMAIN ) );
-		}
-
-		// register deactivation hook only then plugin is activated
-		// and not on every plugin load.
-		register_deactivation_hook( __FILE__, array('Dicentis_Podcast', 'deactivate') );
-
-		// Dipo_RSS::add_podcast_feed();
-		// flush_rewrite_rules();
-	} // END public static function activate()
-
-	/**
-	 * Deactivate the plugin
-	 * @return [type] [description]
-	 */
-	public static function deactivate() {
-		flush_rewrite_rules();
-	} // END public static function deactivate()
-
-	public function single_template( $single_template ) {
-		global $post;
-
-		if ( $post->post_type == Dipo_Podcast_Post_Type::POST_TYPE ) {
-			$single_template = DIPO_TEMPLATES_DIR . '/episode-single.php';
-		}
-
-		return $single_template;
-	}
-
-	public function podcast_archive_template( $archive_template ) {
-		global $post;
-
-		//  if ( is_post_type_archive ( Dicentis_Podcast_CPT::POST_TYPE ) ) {
-		// 	$archive_template = dirname( __FILE__ ) . '/templates/podcast-archive.php';
-		// }
-
-		return $archive_template;
-	}
-
 	public function run() {
 		$this->hook_loader->run();
 	}
+
 }
