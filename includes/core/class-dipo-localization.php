9cb3964c7b0b657c69365572910c4775dfda6670
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2014-07-06 13:11:28 +0200

    Add PHPDoc to Dipo_Localization

--- includes/core/class-dipo-localization.php
+++ includes/core/class-dipo-localization.php
@@ -2,8 +2,17 @@
 
 namespace Dicentis\Core;
 
+/**
+ * Localization class for loading Dicentis' textdomain.
+ *
+ * @author Hans-Helge Buerger <mail@hanshelgebuerger.de>
+ * @version 0.2.0
+ */
 class Dipo_Localization {
 
+	/**
+	 * Loads the plugin textdomain for Dicentis.
+	 */
 	public function load_localisation() {
 
 		load_plugin_textdomain( 'dicentis', false,
