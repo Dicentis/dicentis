ac81febc955917329092133c88064e1fa728e55d
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2014-10-31 21:20:29 +0100

    Change MVC structure. Pass controller to view and model and provide getter function

--- includes/controller/class-dipo-settings-controller.php
+++ includes/controller/class-dipo-settings-controller.php
@@ -19,8 +19,8 @@ class Dipo_Settings_Controller {
 
 	public function __construct() {
 
-		$this->model = new Dipo_Settings_Model();
-		$this->view = new Dipo_Settings_View();
+		$this->view = new Dipo_Settings_View( $this );
+		$this->model = new Dipo_Settings_Model( $this );
 
 		$this->properties = Core\Dipo_Property_List::get_instance();
 		$this->textdomain = $this->properties->get( 'textdomain' );
@@ -33,7 +33,7 @@ class Dipo_Settings_Controller {
 		add_action( 'admin_enqueue_scripts',
 			array( $this->view, 'admin_settings_scripts' ) );
 		add_action('create_podcast_show',
-			array( $this->model, 'register_show_settings' ), 10, 1);
+			array( $this, 'register_show_settings' ), 10, 1);
 	}
 
 	/**
@@ -172,7 +172,7 @@ class Dipo_Settings_Controller {
 			'dipo_itunes_main'
 		);
 
-		$this->model->init_settings();
+		$this->init_settings();
 
 	} // END public function admin_init()
 
@@ -384,4 +384,43 @@ class Dipo_Settings_Controller {
 		array_unshift( $links, $settings_link );
 		return $links;
 	}
+
+	public function init_settings() {
+		$show_model = new \Dicentis\Podcast_Post_Type\Dipo_Podcast_Shows_Model();
+		$shows = $show_model->get_shows( false );
+
+		$this->register_show_settings( -1 );
+		foreach ( $shows as $show ) {
+			$this->register_show_settings( $show->term_id );
+		}
+	}
+
+	public function register_show_settings( $term_id ) {
+		if ( -1 < $term_id ) {
+			$term = get_term( $term_id, 'podcast_show' );
+			$slug = $term->slug;
+		} else {
+			$slug = 'all_shows';
+		}
+
+
+		$option = $this->model->get_option_name( $slug );
+
+		// register settings for new show
+		register_setting(
+			$option,
+			$option,
+			array( $this->model, 'validate_show_options' ) );
+
+		$sec_id = $this->model->add_show_settings_section( $slug );
+	}
+
+	public function get_view() {
+		return $this->view;
+	}
+
+	public function get_model() {
+		return $this->model;
+	}
+
 } // END class Dicentis_Settings
