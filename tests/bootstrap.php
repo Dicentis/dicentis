e448268b586ef63389f0f3d86e7b03089cd6bd45
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-08-03 23:00:25 +0200

    add composer for phpunit and add sample test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,19 +8,19 @@
  */
 
 // Activates this plugin in WordPress so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'dicentis-podcast/dicentis-podcast.php' ),
-);
+//$GLOBALS['wp_tests_options'] = array(
+//	'active_plugins' => array( 'dicentis-podcast/dicentis-podcast.php' ),
+//);
 
 // If the develop repo location is defined (as WP_TESTS_DIR), use that
 // location. Otherwise, we'll just assume that this plugin is installed in a
 // WordPress develop SVN checkout.
 
-if( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	require getenv( 'WP_TESTS_DIR' ) . 'includes/bootstrap.php';
-} else {
-	require '/Users/RetinaObst/Documents/Code/PHP/WordPress/plugins/vagrant-local/www/wordpress-develop/tests/phpunit/includes/bootstrap.php';
-}
+//if( false !== getenv( 'WP_TESTS_DIR' ) ) {
+//	require getenv( 'WP_TESTS_DIR' ) . 'includes/bootstrap.php';
+//} else {
+//	require '/Users/RetinaObst/Documents/Code/PHP/WordPress/plugins/vagrant-local/www/wordpress-develop/tests/phpunit/includes/bootstrap.php';
+//}
 
 if (!defined('DIPO_ROOT')) {
 	define('DIPO_ROOT', dirname( dirname(__FILE__) ) );
